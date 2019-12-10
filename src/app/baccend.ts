51627d2f67f1afa2dfbd001e3ff8b9eac49c0b00
chrisStanulet <43823693+chrisStanulet@users.noreply.github.com>
2019-12-10 18:08:58 -0500

    ok

--- src/app/baccend.ts
+++ src/app/baccend.ts
@@ -12,12 +12,12 @@ export class BackService {
 
   constructor(private httpClient: HttpClient) { }
 
-  SERVER_URL: string = "10.70.6.70"
-  
+  SERVER_URL: string = "http://10.70.6.70/"
+
   public sendMath(data:string) {
-    data=data.split('^').join('**');
+    console.log("sending");
     data=encodeURIComponent(data)
-    let uploadURL = `${this.SERVER_URL}api/solveAlg?equation=`;
+    let uploadURL = `${this.SERVER_URL}api/solveAlg?equation=12x%2B5%3D15x-7`;
     return this.httpClient.post<string>(uploadURL, data);
   }
 }

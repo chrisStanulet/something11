6e7aef74b9d6283c9d8a43a9cd4dc3907157deca
chrisStanulet <43823693+chrisStanulet@users.noreply.github.com>
2019-12-12 19:13:15 -0500

    talk

--- src/app/baccend.ts
+++ src/app/baccend.ts
@@ -13,7 +13,9 @@ export class BackService {
 
   constructor(private httpClient: HttpClient) { }
 
-  SERVER_URL: string = "http://10.70.6.70/api/solveAlg?equation="
+  //SERVER_URL: string = "http://10.70.6.70/api/solveAlg?equation="
+  SERVER_URL: string = "http://192.168.50.233/api/solveAlg?equation="
+  
 
   public sendMath(data:string) {
     console.log("sending");
@@ -24,9 +26,5 @@ export class BackService {
     let stuff = this.httpClient.get<Answer>(uploadURL).subscribe((answer:Answer)=> 
     console.log(answer.ans));
     return stuff;
-  }
-
-  httpcallback(stuff){
-
   }
 }
